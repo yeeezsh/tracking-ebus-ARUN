7324a0f9f2ba9ad209b5c182073c5a7dcfbda985
yee2542 <yee2542gtl@gmail.com>
2019-11-03 03:13:19 +0700

    complete bus interface

--- bus.interface.js
+++ bus.interface.js
@@ -1,16 +1,25 @@
 const axios = require('axios')
 const target = 'https://www.aismagellan.io/api/things/pull/4bddf720-fc72-11e9-96dd-9fb5d8a71344'
 
-function parse () {
-    return ''
+function parse(parse, override) {
+    return {
+        busName: override.busName,
+        lat: parse.Location[0],
+        lng: parse.Location[1],
+        speed: parse['Speed MPH'],
+        time: new Date(),
+    }
 }
 
-async function busRequest () {
-const {data} = axios.default.get(target)
-console.log(data)
-return data
+async function busRequest(override) {
+    const { data } = await axios.default.get(target)
+    const parsed = parse(data, override)
+    return parsed
 }
-module.exports = () => {
-const api = setInterval(() => busRequest, 500)
 
+module.exports = (override, cb) => {
+    return setInterval(async () => {
+        const data = await busRequest(override)
+        return cb(data)
+    }, 500)
 }
